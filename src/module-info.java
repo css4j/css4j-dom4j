40f323c0f2b6709d148e31fd17cdace15ba3c629
Carlos Amengual <carlosame644+github@gmail.com>
2020-09-12 17:54:56 +0200

    module: as the agent.net dependency is static, put a transitive dependency from css4j core.

--- src/module-info.java
+++ src/module-info.java
@@ -12,6 +12,7 @@
 module io.sf.carte.css4j.dom4j {
 	exports io.sf.carte.doc.dom4j;
 
+	requires transitive io.sf.carte.css4j;
 	requires static io.sf.carte.css4j.agent.net;
 	requires transitive dom4j;
 	requires static xpp3;
